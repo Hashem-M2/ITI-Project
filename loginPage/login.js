7f733696af03bf1411ec8d385764936caeb9e53c
Hashem-M2 <117672011+Hashem-M2@users.noreply.github.com>
2023-10-01 15:41:02 +0300

    Update login.js 3

--- loginPage/login.js
+++ loginPage/login.js
@@ -1,7 +1,7 @@
 var btn = document.getElementById("btt");
 
 function redirect() {
-  location.assign("./index.html");
+  location.assign("./ITI-Project/index.html");
 }
 
 var userid = document.getElementById("userid");
